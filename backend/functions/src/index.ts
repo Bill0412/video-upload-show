86deb9988711873d21d70e38a5dd100889ffc9da
fenghe <jxphxufh@gmail.com>
2020-03-16 22:37:29 +0800

    feat: stably show 1 video

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -1,14 +1,25 @@
 import * as functions from 'firebase-functions';
 
-const os = require('os');
-const path = require('path');
+// const os = require('os');
+// const path = require('path');
 const cors = require('cors')({origin: true});
-const Busboy = require('busboy');
-const fs = require('fs');
-const gcs = require('@google-cloud/storage')();
+// const fs = require('fs');
+// const gcs = require('@google-cloud/storage')();
 
+
+/*
+ * Generate a JSON File of Download URLs of all the videos in the Firebase  Storage
+ * Download URL Format: {event.mediaLink}&token={event.metadata.firebaseStorageDownloadTokens}
+ */
 export const onFileChange = functions.storage.object().onFinalize((event: any) => {
     console.log(event);
+
+    const tmp = event.selfLink.parse('/');
+    const filename = tmp[tmp.length - 1];
+    const link = "https://firebasestorage.googleapis.com/v0/b/video-c34ea.appspot.com/o/" + filename + "?alt=media&token=" + event.metadata.firebaseStorageDownloadTokens;
+
+    console.log(link);
+
     return;
 });
 
@@ -21,7 +32,8 @@ export const getVideoURL = functions.https.onRequest((req, res) => {
                 message: 'Not allowed'
             });
         }
-        
+        // try to traverse the whole directory
+
         // get the urls JSON File
 
         return res.status(200).json({
